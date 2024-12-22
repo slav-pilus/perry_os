5218ac3d5024310daaa9ddc1340299ecb7de8f99
Slav Pilus <slav@pilus.me.uk>
2024-12-22 22:07:46 +0000

    Added VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 #![no_std]
 #![no_main]
+mod vga_buffer;
 
+use crate::vga_buffer::Color::*;
+use crate::vga_buffer::WRITER;
+use core::ops::Deref;
 use core::panic::PanicInfo;
-
-static GREETING: &[u8] = b"Welcome to Perry OS!";
+use vga_buffer::ColorCode;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    WRITER.deref().lock().set_color(ColorCode::new(Red, Black));
+    println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+    println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+    println!("::'########::'########:'########::'########::'##:::'##::::::'#######:::'######::");
+    println!("::##.... ##: ##.....:: ##.... ##: ##.... ##:. ##:'##::::::'##.... ##:'##... ##::");
+    println!("::##:::: ##: ##::::::: ##:::: ##: ##:::: ##::. ####::::::: ##:::: ##: ##:::..:::");
+    println!("::########:: ######::: ########:: ########::::. ##:::::::: ##:::: ##:. ######:::");
+    println!("::##.....::: ##...:::: ##.. ##::: ##.. ##:::::: ##:::::::: ##:::: ##::..... ##::");
+    println!("::##:::::::: ##::::::: ##::. ##:: ##::. ##::::: ##:::::::: ##:::: ##:'##::: ##::");
+    println!("::##:::::::: ########: ##:::. ##: ##:::. ##:::: ##::::::::. #######::. ######:::");
+    println!("::..:::::::::........::..:::::..::..:::::..:::::..::::::::::.......::::......:::");
+    println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+    println!("\n\n\n\n\n");
+    WRITER.deref().lock().set_color(ColorCode::new(Green, Black));
+    println!("All systems up and running!");
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in GREETING.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
-/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
